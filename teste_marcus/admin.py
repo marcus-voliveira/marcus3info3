297e92e2122d539fc1aeebed86feed86f800c06c
Marcus Oliveira <marcus.oliveira2403@gmail.com>
2023-03-03 15:09:00 -0300

    teste_marcus3info3

---
+++ teste_marcus/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from django.contrib import admin
+
+from .models import Categoria
+
+admin.site.register(Categoria)
+
+from django.contrib import admin
+
+from .models import Categoria
+
+admin.site.register(Categoria)
