adc2a83322b4024f89f97dbf81427be6921468a4
Matheus Gobetti <gobetti.ifc@gmail.com>
2023-03-06 16:55:03 -0300

    segundo commit

--- teste_marcus/admin.py
+++ teste_marcus/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
 
-from django.contrib import admin
-
-from .models import Categoria
+from .models import Categoria, Editora
 
 admin.site.register(Categoria)
-
-from django.contrib import admin
-
-from .models import Categoria
-
-admin.site.register(Categoria)
+admin.site.register(Editora)
